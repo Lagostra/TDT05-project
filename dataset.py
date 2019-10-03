57aa42427d8a62f33509a287345404d1a98f892b
Eivind Lie Andreassen <ela4991@gmail.com>
2019-10-03 14:44:20 +0200

    Add dataset and preprocessing scripts

--- dataset.py
+++ dataset.py
@@ -1,6 +1,26 @@
-import urllib
+import urllib.request
+import pandas as pd
+import os
+import kaggle
 
 train_url = 'https://www.kaggle.com/c/santander-customer-transaction-prediction/download/train.csv'
 test_url = 'https://www.kaggle.com/c/santander-customer-transaction-prediction/download/train.csv'
 
-urllib.request.urlretrieve(train_url, 'data/test.csv')
+def load_data():
+    """if not os.path.isfile('data/train.csv') or not os.path.isfile('data/train.csv'):
+        if not os.path.exists('data'):
+            os.makedirs('data')
+        
+        kaggle.api.authenticate()
+        
+        os.chdir('data')
+        kaggle.api.competitions_data_download_file('santander-customer-transaction-prediction', 'test.csv')
+        os.chdir('..')"""
+    
+    train_set = pd.read_csv('data/train.csv')
+    test_set = pd.read_csv('data/test.csv')
+
+    return train_set, test_set
+
+if __name__ == '__main__':
+    get_data()
--- preprocessing.py
+++ preprocessing.py
@@ -3,4 +3,49 @@ import numpy as np
 from sklearn import preprocessing
 from imblearn.over_sampling import SMOTE
 
-train_set = pd.read_csv('/data/')
+def preprocess(data_set, reduce_size=-1, balance_targets=True, target_col='target', add_features=['sum', 'mean', 'min', 'max', 'std', 'median', 'skew', 'kurt']):
+    '''
+        Preprocesses the data set contained in the DataFRame data_set.
+    '''
+
+    if reduce_size > 0:
+        reduce(data_set, reduce_size)
+    
+    if balance_targets:
+        balance(data_set, target_col)
+    
+    if add_features:
+        feature_enhancement(data_set, add_features, target_col)
+
+
+def reduce(data_set, reduce_size):
+    sample_frac = reduce_size / data_set.shape[0]
+    reduced = train_set.groupby('target', group_keys=False).apply(lambda x: x.sample(frac=sample_frac))
+    return reduced
+
+def balance(data_set, target_col):
+    sm = SMOTE(sampling_strategy='minority', random_state=42)
+    oversampled_x, oversampled_y = sm.fit_sample(train_data.drop(target_col, axis=1), train_data[target_col])
+    oversampled = pd.concat([pd.DataFrame(oversampled_x), pd.DataFrame(oversampled_y)], axis=1)
+    oversampled.columns = data_set.columns
+    return oversampled
+
+def feature_enhancement(data_set, enhanced_features, target_col):
+    features = data_set.drop(target_col)
+    
+    if 'sum' in enhanced_features:
+        data_set['sum'] = features.sum(axis=1)
+    if 'mean' in enhanced_features:
+        data_set['mean'] = features.mean(axis=1)
+    if 'min' in enhanced_features:
+        data_set['min'] = features.min(axis=1)
+    if 'max' in enhanced_features:
+        data_set['max'] = features.max(axis=1)
+    if 'std' in enhanced_features:
+        data_set['std'] = features.std(axis=1)
+    if 'median' in enhanced_features:
+        data_set['median'] = features.median(axis=1)
+    if 'skew' in enhanced_features:
+        data_set['skew'] = features.skew(axis=1)
+    if 'kurt' in enhanced_features:
+        data_set['kurt'] = features.kurtn(axis=1)
